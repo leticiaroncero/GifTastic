8f5411238d956f43f783117bd0f635ad13ab8862
Leticia Roncero <53988147+leticiaroncero@users.noreply.github.com>
2019-10-31 14:01:49 -0700

    add new logic to create buttons for existing topics, and clear input

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,26 @@ var topics = ["halloween", "pumpkin", "candy", "ghost"];
 
 var buttonContainer = $("#button-container");
 
+
 for (var i = 0; i < topics.length; i++) {
     var gifButton = $("<button>").text(topics[i]);
+    buttonContainer.append(gifButton)
+};
+
+
+$("#gif-submit").on("click", function (event) {
+    event.preventDefault();
+    var gifInput = $("#gif-search").val().trim();
+    if (gifInput.length !== 0) {
+        topics.push(gifInput);
+        newButton(gifInput);
+        $("#gif-search").val("")
+    }
+});
+
+function newButton(input) {
+    var gifButton = $("<button>").text(input);
     buttonContainer.append(gifButton);
-}
+};
+
+
